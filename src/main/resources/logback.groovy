bae0361ea90cb105d5db102738b6e8f8673ac108
Theophile Meneau <tmeneau@xetus.com>
2016-03-01 13:46:50 -0500

    fix incorrect logback.groovy duplication
    While configuring logback-test.xml files is supported, no such support
    is extended for logback.groovy files. The recommended way to implement
    test-specific logback configurations is actually to embed conditional
    logic for the tests into a the main logback.groovy file on the
    classpath.
    
    Using the recommended solution has the nice side benefit of removing
    some false Eclipse errors.

--- src/main/resources/logback.groovy
+++ src/main/resources/logback.groovy
@@ -2,18 +2,24 @@ import ch.qos.logback.core.ConsoleAppender
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder
 import static ch.qos.logback.classic.Level.*
 
+
 appender("CONSOLE", ConsoleAppender) {
   encoder(PatternLayoutEncoder) {
     pattern = "%d{yyyy-MM-dd HH:mm:ss.SSS} [%thread] [%X{req.xForwardedFor}] %-5level %logger{0} - %msg%n"
   }
 }
 
-/*
- * CXF logs requests to un-serviced endpoints and client disconnects as "WARN",
- * which can make things a little verbose
- */
-logger("org.apache.cxf.jaxrs.interceptor.JAXRSInInterceptor", ERROR)
-logger("org.apache.cxf.phase.PhaseInterceptorChain", ERROR)
-logger("org.apache.cxf.jaxrs.impl.WebApplicationExceptionMapper", ERROR)
-
-root(INFO, ["CONSOLE"])
+if (System.getProperty("DISABLE_LOGGING")) {
+  root(OFF, ["CONSOLE"])
+} else {
+  
+  /*
+   * CXF logs requests to un-serviced endpoints and client disconnects as "WARN",
+   * which can make things a little verbose
+   */
+  logger("org.apache.cxf.jaxrs.interceptor.JAXRSInInterceptor", ERROR)
+  logger("org.apache.cxf.phase.PhaseInterceptorChain", ERROR)
+  logger("org.apache.cxf.jaxrs.impl.WebApplicationExceptionMapper", ERROR)
+  
+  root(INFO, ["CONSOLE"])
+}
